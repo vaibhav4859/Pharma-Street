1822c287725cb1c76937f433a6fc82d4129219f2
vaibhav <vsachdeva4859@gmail.com>
2023-10-30 21:45:45 +0530

    Hosted

--- src/Firebase.js
+++ src/Firebase.js
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDXmFKxdzHV_WWDzDsPLYsMDXH3Xln267k",
-    authDomain: "pharma-street.firebaseapp.com",
-    databaseURL: "https://pharma-street-default-rtdb.firebaseio.com",
-    projectId: "pharma-street",
-    storageBucket: "pharma-street.appspot.com",
-    messagingSenderId: "910430484237",
-    appId: "1:910430484237:web:c9d023c1e9a7bfa2222244"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
